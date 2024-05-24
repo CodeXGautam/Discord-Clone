87a40ba0fe9809de7de50b217a0c5613d42dd46f
himanshu <himanshu@himanshus-MacBook-Air.local>
2024-05-24 12:33:25 +0530

    Files added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "poetsen":['Poetsen One','sans-serif'],
+        "trirong":['Trirong','sans-serif']
+
+      }
+    },
+  },
+  plugins: [],
+}
+
